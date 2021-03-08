668ac50f2e5aa13d30316fa8b5d571b741e3edf1
Aniux2807 <79299383+Aniux2807@users.noreply.github.com>
2021-03-08 02:39:58 -0600

    Add files via upload

---
+++ Form3.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace Lab_2_repaso
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textNIT = new System.Windows.Forms.TextBox();
+            this.textLicensePlate = new System.Windows.Forms.TextBox();
+            this.textKilometers = new System.Windows.Forms.TextBox();
+            this.calendarRental = new System.Windows.Forms.MonthCalendar();
+            this.calendarReturn = new System.Windows.Forms.MonthCalendar();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelKilometers = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NIT";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(105, 91);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 65);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 39);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "License plate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(397, 13);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Rental date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(701, 13);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Return date";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(714, 216);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "kilometraje";
+            // 
+            // textNIT
+            // 
+            this.textNIT.Location = new System.Drawing.Point(105, 10);
+            this.textNIT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textNIT.Name = "textNIT";
+            this.textNIT.Size = new System.Drawing.Size(198, 20);
+            this.textNIT.TabIndex = 6;
+            // 
+            // textLicensePlate
+            // 
+            this.textLicensePlate.Location = new System.Drawing.Point(105, 39);
+            this.textLicensePlate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textLicensePlate.Name = "textLicensePlate";
+            this.textLicensePlate.Size = new System.Drawing.Size(198, 20);
+            this.textLicensePlate.TabIndex = 7;
+            // 
+            // textKilometers
+            // 
+            this.textKilometers.Location = new System.Drawing.Point(677, 245);
+            this.textKilometers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textKilometers.Name = "textKilometers";
+            this.textKilometers.Size = new System.Drawing.Size(198, 20);
+            this.textKilometers.TabIndex = 8;
+            // 
+            // calendarRental
+            // 
+            this.calendarRental.Location = new System.Drawing.Point(325, 37);
+            this.calendarRental.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.calendarRental.Name = "calendarRental";
+            this.calendarRental.TabIndex = 9;
+            // 
+            // calendarReturn
+            // 
+            this.calendarReturn.Location = new System.Drawing.Point(609, 37);
+            this.calendarReturn.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.calendarReturn.Name = "calendarReturn";
+            this.calendarReturn.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 160);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(299, 207);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Imprint MT Shadow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(445, 287);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 19);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "kilometros";
+            // 
+            // labelKilometers
+            // 
+            this.labelKilometers.AutoSize = true;
+            this.labelKilometers.Location = new System.Drawing.Point(446, 320);
+            this.labelKilometers.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelKilometers.Name = "labelKilometers";
+            this.labelKilometers.Size = new System.Drawing.Size(94, 13);
+            this.labelKilometers.TabIndex = 13;
+            this.labelKilometers.Text = "-----------------------------";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(886, 368);
+            this.Controls.Add(this.labelKilometers);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.calendarReturn);
+            this.Controls.Add(this.calendarRental);
+            this.Controls.Add(this.textKilometers);
+            this.Controls.Add(this.textLicensePlate);
+            this.Controls.Add(this.textNIT);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textNIT;
+        private System.Windows.Forms.TextBox textLicensePlate;
+        private System.Windows.Forms.TextBox textKilometers;
+        private System.Windows.Forms.MonthCalendar calendarRental;
+        private System.Windows.Forms.MonthCalendar calendarReturn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelKilometers;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# vehiculos
-programa sobre ventas vehiculos 
+# Automobile_Company
+In this program JSON, OOP, lists and MDI forms were implemented in C #
